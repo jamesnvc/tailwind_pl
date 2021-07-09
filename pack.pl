2c5e7ae223e7ca46c55d08afc0da34f525fce750
James N. V. Cash <james.nvc@gmail.com>
2021-07-09 19:05:12 -0400

    Add dependency for inotify

--- pack.pl
+++ pack.pl
@@ -1,8 +1,9 @@
 name(tailwind_pl).
 title('Tailwind-style CSS generator for Prolog').
-version('1.0.2').
+version('1.0.3').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/tailwind_pl').
 download('https://github.com/jamesnvc/tailwind_pl/releases/*.zip').
 provides(tailwind).
 requires(css_write).
+requires(inotify).
