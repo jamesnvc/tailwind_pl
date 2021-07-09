4ddc1d3380a876ec5e32bd24c854f9afcfd2e5be
James N. V. Cash <james.nvc@gmail.com>
2021-07-09 13:08:02 -0400

    Change pack name

--- pack.pl
+++ pack.pl
@@ -1,7 +1,8 @@
-name(tailwind).
+name(tailwind_pl).
 title('Tailwind-style CSS generator for Prolog').
 version('1.0.0').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/tailwind_pl').
 download('https://github.com/jamesnvc/tailwind_pl/releases/*.zip').
+provides(tailwind).
 requires(css_write).
