d0514e607177cf4a9e69ca60b3f9207f57f62ceb
James N. V. Cash <james.nvc@gmail.com>
2021-07-15 19:15:29 -0400

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(tailwind_pl).
 title('Tailwind-style CSS generator for Prolog').
-version('1.0.5').
+version('1.1.0').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/tailwind_pl').
 download('https://github.com/jamesnvc/tailwind_pl/releases/*.zip').
