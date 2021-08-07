47bc49ee3a22c3fc5971feedc78a82137304c264
James N. V. Cash <james.nvc@gmail.com>
2021-08-07 12:33:20 -0400

    Split main library into just watcher
    The actually generation is now coming from tailwind_pl_generate

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(tailwind_pl).
 title('Tailwind-style CSS generator for Prolog').
-version('1.1.1').
+version('2.0.0').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/tailwind_pl').
 download('https://github.com/jamesnvc/tailwind_pl/releases/*.zip').
 provides(tailwind).
-requires(css_write).
+requires(tailwind_pl_generate).
 requires(inotify).
